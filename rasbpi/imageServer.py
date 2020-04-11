5e440392262819c6e8a1163f454006ef428776ef
Your Name <you@example.com>
2020-04-11 20:40:28 +0100

    Working on v3 logic

--- rasbpi/imageServer.py
+++ rasbpi/imageServer.py
@@ -69,7 +69,6 @@ while True:
     except KeyError:
         send_error("Command %s not recognised" % command)
 
-    socket.recv_json()  # Wait for ack
     # Commands returning None have no further execution
     if result is None:
         continue
